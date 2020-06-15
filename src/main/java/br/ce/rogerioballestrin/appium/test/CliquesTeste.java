8e34334ec7f79706c2d40f7a8dba696832f3c6af
Rogerio Ballestrin <ballestrin.rogerio@gmail.com>
2020-06-14 21:21:32 -0300

    Teste com clique duplo

--- src/main/java/br/ce/rogerioballestrin/appium/test/CliquesTeste.java
+++ src/main/java/br/ce/rogerioballestrin/appium/test/CliquesTeste.java
@@ -1,6 +1,7 @@
 package br.ce.rogerioballestrin.appium.test;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.ce.rogerioballestrin.appium.core.BaseTest;
@@ -12,14 +13,25 @@ public class CliquesTeste extends BaseTest {
 	private MenuPage menu = new MenuPage();
 	private CliquePage page = new CliquePage();
 	
+	@Before
+	public void Setup() {
+		menu.acessarCliques();
+	}
+	
 	@Test
 	public void deveRealizarCliqueLongo() {
-		// acessar menu
-		menu.acessarCliques();
 		//clique longo
 		page.cliqueLongo();
 		//verificar texto
 		Assert.assertEquals("Clique Longo", page.obterTextoCampo());
 	}
+	
+	@Test
+	public void deveRealizarCliqueDuplo() {
+		page.clicarPorTexto("Clique duplo");
+		page.clicarPorTexto("Clique duplo");
+		Assert.assertEquals("Duplo Clique", page.obterTextoCampo());
+	}
+	
 
 }
